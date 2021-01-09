6085cd028e4594e3d39f13f00643de1e38abd1fd
abc <selcan.orudjeva@gmail.com>
2021-01-09 16:17:29 +0400

    firstjs

---
+++ js/math-function.js
@@ -0,0 +1,35 @@
+function Topla(c, d) {
+    console.log(c+d);
+}
+function Cix(c,d) {
+    console.log(c-d);
+}
+function Vurma(c,d) {
+    console.log(c*d);
+}
+function Bolme(c,d) {
+    console.log(c/d);
+}
+function KvadrataYukseltme(c) {
+    return c*c;
+}
+function KubaYukseltme(d) {
+    return (Math.pow(3,3));
+}
+function QaliqHesablama(c,d) {
+    console.log(c%d);
+}
+
+Topla(4,7);
+Cix(12,5);
+Vurma(2,2);
+Bolme(81,9);
+QaliqHesablama(6,8);
+    console.log(KvadrataYukseltme(3));
+    console.log(KubaYukseltme(3));
+
+
+
+
+
+
